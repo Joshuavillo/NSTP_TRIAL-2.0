8b0c20d7401d7014b5e9aed0a8d3075ae79eb6ae
Joshua Niño A. Villorejo <joshuaninovillorejo131@gmail.com>
2026-04-12 20:59:45 +0800

    w

---
+++ data.js
@@ -0,0 +1,15 @@
+const gridData = [
+  { id: 1, image: '../Guide GO Pictures/collegeReg.jpg', title: 'COLLEGE REGISTRAR', details: '- Kilid sa amoa.' },
+  { id: 2, image: '../Guide GO Pictures/studAccDiv.jpg', title: 'ACCOUNTING', details: '' },
+  { id: 3, image: '../UC MVH/ucGoals.png', title: 'BOOK STORE', details: 'Strategic goals and objectives of UC to achieve excellence in teaching and research.' },
+  { id: 4, image: '../UC MVH/ucHymn.png', title: 'NSTP OFFICE', details: 'Lyrics and history behind the University of Cabuyao hymn.' },
+  { id: 5, image: '../CCS MV/ccsMission.png', title: 'STUDENT AFFAIR OFFICE', details: 'College of Computer Studies mission - producing competent IT professionals.' },
+  { id: 6, image: '../CCS MV/ccsVision.png', title: 'MAIN LIBRARY', details: 'Vision of CCS to be a leading IT college in the region.' },
+  { id: 7, image: '../CCS MV/ccsGoals.png', title: 'PSITS OFFICE', details: 'Goals of CCS department for student development and industry readiness.' },
+  { id: 8, image: '../UC MVH/ucMission.png', title: 'FACULTY OFFICE', details: 'Navigation and facilities guide for UC campus buildings and services.' },
+  { id: 9, image: '../UC MVH/ucVision.png', title: 'Tech Facilities #9', details: 'Guide to technology labs, computer centers, and innovation hubs.' },
+  { id: 10, image: '../CCS MV/ccsMission.png', title: 'Library Guide #10', details: 'Digital and physical library resources for students and faculty.' },
+  { id: 11, image: '../CCS MV/ccsVision.png', title: 'Services Guide #11', details: 'Student services, counseling, and support systems available.' },
+  { id: 12, image: '../UC MVH/ucGoals.png', title: 'Events Guide #12', details: 'Upcoming campus events, seminars, and tech workshops calendar.' }
+];
+
