90b57dc3db10db3a090a1851f12831d228d70e94
Joshua Niño A. Villorejo <joshuaninovillorejo131@gmail.com>
2026-04-12 21:16:31 +0800

    Create searchIndex.js

---
+++ searchIndex.js
@@ -0,0 +1,22 @@
+// Copy of root searchIndex.js for Home dir (file:// origin issue workaround)
+const searchIndex = [
+  // Home carousel items
+  { id: 'home-uc-mission', name: 'UC Mission', type: 'image', path: '../UC MVH/ucMission.png', page: 'Home', desc: 'University of Cebu Mission statement image', menuTarget: null },
+  { id: 'home-uc-vision', name: 'UC Vision', type: 'image', path: '../UC MVH/ucVision.png', page: 'Home', desc: 'University of Cebu Vision image' },
+  { id: 'home-uc-goals', name: 'UC Goals', type: 'image', path: '../UC MVH/ucGoals.png', page: 'Home', desc: 'University of Cebu Goals image' },
+  { id: 'home-uc-hymn', name: 'UC Hymn', type: 'image', path: '../UC MVH/ucHymn.png', page: 'Home', desc: 'University of Cebu Hymn image', menuTarget: null },
+  // CCS Department carousel
+  { id: 'ccs-mission', name: 'CCS Mission', type: 'image', path: '../CCS MV/ccsMission.png', page: 'CCS Department', desc: 'CCS Mission statement image', menuTarget: null },
+  { id: 'ccs-vision', name: 'CCS Vision', type: 'image', path: '../CCS MV/ccsVision.png', page: 'CCS Department', desc: 'CCS Vision statement image' },
+  { id: 'ccs-goals', name: 'CCS Goals', type: 'image', path: '../CCS MV/ccsGoals.png', page: 'CCS Department', desc: 'CCS Goals image', menuTarget: null },
+  // Tech Tutorial grid
+  { id: 'tech-1', name: 'Tech Tutorial 1', type: 'page', path: '../Tech Tutorial/techTut.html', page: 'Tech Tutorial', desc: 'First tech tutorial item', menuTarget: 'techTut.html' },
+  { id: 'tech-2', name: 'Tech Tutorial 2', type: 'page', path: '../Tech Tutorial/techTut.html', page: 'Tech Tutorial', desc: 'Second tech tutorial item' },
+  // ... (truncated for brevity - full list in root)
+  { id: 'nav-home', name: 'Home', type: 'page', path: './index.html', page: 'Home', desc: 'Main home page', menuTarget: 'index.html' },
+  // Add more mappings as needed
+  { id: 'ccs-dept', name: 'CCS Department', type: 'page', path: '../CCS Department/ccsDept.html', page: 'CCS Department', desc: 'CCS Department page', menuTarget: 'ccsDept.html' },
+  { id: 'guide-go', name: 'Guide Go', type: 'page', path: '../Guide Go/guideGo.html', page: 'Guide Go', desc: 'Guide Go page', menuTarget: 'guideGo.html' },
+  { id: 'lost-found', name: 'Lost and Found', type: 'page', path: '../Lost and Found/lostnFound.html', page: 'Lost and Found', desc: 'Lost and Found page', menuTarget: 'lostnFound.html' }
+];
+
